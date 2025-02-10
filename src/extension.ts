9623c1ea0c488f1c16d3ca5a62d98f79092a5092
Dani Guardiola <hi@daniguardio.la>
2025-02-10 14:16:40 +0100

    add support for diffing examples

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as path from "node:path";
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext): void {
-	const disposable = vscode.commands.registerCommand(
+	const disposableSource = vscode.commands.registerCommand(
 		"ariakit-solid.openPortDiff",
 		(uri: vscode.Uri) => {
 			if (!uri || !uri.fsPath) {
@@ -48,7 +48,51 @@ export function activate(context: vscode.ExtensionContext): void {
 		},
 	);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableSource);
+
+	const disposableExamples = vscode.commands.registerCommand(
+		"ariakit-solid.openExampleDiff",
+		(uri: vscode.Uri) => {
+			if (!uri || !uri.fsPath) {
+				vscode.window.showErrorMessage("No file selected.");
+				return;
+			}
+
+			const filePath: string = uri.fsPath;
+			const workspaceFolder: string | undefined =
+				vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+			if (!workspaceFolder) {
+				vscode.window.showErrorMessage("No workspace folder found.");
+				return;
+			}
+
+			const relativePath: string = path.relative(
+				path.join(workspaceFolder, "examples"),
+				filePath,
+			);
+			if (!relativePath.includes(".solid.")) {
+				vscode.window.showErrorMessage(
+					"File is not a .solid.ts or .solid.tsx file.",
+				);
+				return;
+			}
+
+			const leftFilePath: string = filePath.replace(".solid.", ".react.");
+			const rightFilePath: string = filePath;
+
+			const leftUri: vscode.Uri = vscode.Uri.file(leftFilePath);
+			const rightUri: vscode.Uri = vscode.Uri.file(rightFilePath);
+
+			vscode.commands.executeCommand(
+				"vscode.diff",
+				leftUri,
+				rightUri,
+				`Example Diff: ${path.basename(rightFilePath)}`,
+			);
+		},
+	);
+
+	context.subscriptions.push(disposableExamples);
 
 	vscode.commands.registerCommand("ariakit-solid.refresh", () => {
 		vscode.commands.executeCommand(
